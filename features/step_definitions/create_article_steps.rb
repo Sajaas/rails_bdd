1a52d7e126c4a3eeb1ea3a502708325faa55e413
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-18 14:01:17 +0200

    added test for listing articles, working on test for adding articles

---
+++ features/step_definitions/create_article_steps.rb
@@ -0,0 +1,19 @@
+Given(/^I visit the "([^"]*)" page$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click "([^"]*)" link$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click "([^"]*)" button$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be on "([^"]*)" page$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
