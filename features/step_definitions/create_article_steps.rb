5cef399a48f85528882a55f0085b93a568501a62
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-19 09:47:27 +0200

    more create article steps

--- features/step_definitions/create_article_steps.rb
+++ features/step_definitions/create_article_steps.rb
@@ -2,18 +2,18 @@ Given(/^I visit the "([^"]*)" page$/) do |landing|
   visit root_path
 end
 
-When(/^I click "([^"]*)" link$/) do |new_article|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I click "([^"]*)" link$/) do |link_name|
+  click_link_or_button link_name
 end
 
 When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |title, content|
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in title, with: content
 end
 
-When(/^I click "([^"]*)" button$/) do |create_article|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I click "([^"]*)" button$/) do |button|
+  click_button button
 end
 
 Then(/^I should be on "([^"]*)" page$/) do |new_article_page|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_current_path new_article_page
 end
