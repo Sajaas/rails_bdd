6702d19d30216d46e7d7c9bf1448a93e89281290
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-19 08:58:47 +0200

    some steps added

--- features/step_definitions/create_article_steps.rb
+++ features/step_definitions/create_article_steps.rb
@@ -1,19 +1,19 @@
-Given(/^I visit the "([^"]*)" page$/) do |arg1|
+Given(/^I visit the "([^"]*)" page$/) do |landing|
+  visit root_path
+end
+
+When(/^I click "([^"]*)" link$/) do |new_article|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^I click "([^"]*)" link$/) do |arg1|
+When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |title, content|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
+When(/^I click "([^"]*)" button$/) do |create_article|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^I click "([^"]*)" button$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^I should be on "([^"]*)" page$/) do |arg1|
+Then(/^I should be on "([^"]*)" page$/) do |new_article_page|
   pending # Write code here that turns the phrase above into concrete actions
 end
