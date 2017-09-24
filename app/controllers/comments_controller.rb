322e8f3f82880684d280ecd0e9fd05edb378ac69
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-24 17:34:58 +0200

    gets error if no title in update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,13 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
+    # @comment = Comment.new(comment_params)
+    # if @comment.save
+    # redirect_to article_path(@article)
+    # else
+    #   render 'articles/show'
+    # end
+
   end
 
   private
